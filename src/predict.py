d42fb90b99abcfffa1cc77c0fc1c8da3268a738b
Akshita Baweja. <Akshita.101692@stu.upes.ac.in>
2025-08-24 14:19:42 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -5,8 +5,9 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 # Load trained model
 model = tf.keras.models.load_model("gecko_model.h5")
 
-# Path to the test image
-image_path = r"C:\Users\bawej\Downloads\gecko_svm_starter\gettyimages-130865154-612x612.jpg"
+# Path to the test image(Copy the path of whichever image you want to add)
+image_path = r"C:\Users\bawej\Downloads\gecko_svm_starter\aug_0_418.jpg"
+
 
 # Define class labels (same order as your training folders)
 class_labels = [
